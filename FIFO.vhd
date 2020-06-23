863ac433be729f5632fcf22adc9a34ebbf468793
ranamagdi <52126542+ranamagdi@users.noreply.github.com>
2020-06-23 18:47:43 +0200

    Add files via upload

---
+++ FIFO.vhd
@@ -0,0 +1,109 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:59:48 05/13/2020 
+-- Design Name: 
+-- Module Name:    FIFO - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+
+entity FIFO is
+generic ( width: integer:=8  );
+
+    Port ( reset : in  std_logic;
+           rclk : in std_logic;
+           wclk : in  std_logic;
+           rreq : in  std_logic;
+           wreq : in std_logic;
+           datain : in std_logic_vector(width-1 downto 0);
+           dataout : out std_logic_VECTOR(width-1 downto 0);
+           empty : out  std_logic;
+           full : out  std_logic);
+end FIFO;
+
+architecture Behavioral of FIFO is
+type FIFO_Memory is array (0 to 15) of std_logic_VECTOR (width - 1 downto 0);
+signal Memory : FIFO_Memory :=(others => (others => '0'));   
+signal empty_s,full_s :std_logic:='0';
+signal r_ptr, w_ptr : integer :=0;
+
+
+begin
+empty<=empty_s;
+ full<=full_s; 
+ 
+p:process (rclk,wclk ,reset)
+variable counter : integer := 0; 
+
+begin
+   
+if (reset ='1')then
+dataout <=(others => '0');
+empty_s <='1';
+full_s <='0';
+r_ptr<=0;
+w_ptr<=0;
+counter:=0;
+else
+if(rising_edge(wclk)) then
+if(wreq='1' and full_s ='0' ) then
+  -- Write Data to Memory
+      Memory(w_ptr)<= datain;
+		w_ptr<=w_ptr+1;
+		counter:=counter+1;
+		end if;
+	   end if;
+		  
+if(rising_edge(rclk) )then	  
+ if(rreq='1' and empty_s ='0' ) then
+  -- Update data output
+		dataout <= Memory(r_ptr)  ;
+		r_ptr <= r_ptr+1;
+		counter:=counter-1;
+		end if;
+		end if;
+		 if(w_ptr = 15) then      
+        w_ptr <= 0;
+        end if;
+		  
+		if(r_ptr = 15) then      
+       r_ptr <= 0;
+        end if;
+		  
+		 
+	   --setting empty and full flags.
+        if(counter = 0) then
+            empty_s <= '1';
+        else
+            empty_s <= '0';
+        end if;
+        if(counter = 16) then
+            full_s <= '1';
+        else
+            full_s <= '0';
+        end if;
+    end if; 
+	 
+	 
+
+    end process;
+						
+
+
+end Behavioral;
+
---
+++ FIFO_test.vhd
@@ -0,0 +1,139 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   02:38:10 05/14/2020
+-- Design Name:   
+-- Module Name:   D:/FIFO/FIFO_test.vhd
+-- Project Name:  FIFO
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: FIFO
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+
+ENTITY FIFO_test IS
+END FIFO_test;
+ 
+ARCHITECTURE behavior OF FIFO_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT FIFO
+    PORT(
+         reset : IN  std_logic;
+         rclk : IN   std_logic;
+         wclk : IN   std_logic;
+         rreq : IN   std_logic;
+         wreq : IN   std_logic;
+         datain : IN std_logic_vector(7 downto 0);
+         dataout : out std_logic_vector(7 downto 0);
+         empty : out std_logic;
+         full : out std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal reset :std_logic := '0';
+   signal rclk : std_logic := '0';
+   signal wclk : std_logic := '0';
+   signal rreq : std_logic := '0';
+   signal wreq : std_logic := '0';
+   signal datain : std_logic_vector(7 downto 0) := (others => '0');
+   signal dataout : std_logic_vector(7 downto 0) := (others => '0');
+   signal empty : std_logic := '0';
+   signal full : std_logic := '0';
+	--temporary signals
+    signal i : integer := 0;
+
+   -- Clock period definitions
+   constant rclk_period : time := 10 ns;
+   constant wclk_period : time := 10 ns;
+	constant depth : integer := 16;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: FIFO PORT MAP (
+          reset => reset,
+          rclk => rclk,
+          wclk => wclk,
+          rreq => rreq,
+          wreq => wreq,
+          datain => datain,
+          dataout => dataout,
+          empty => empty,
+          full => full
+        );
+
+   -- Clock process definitions
+   rclk_process :process
+   begin
+		rclk <= '0';
+		wait for rclk_period/2;
+		rclk <= '1';
+		wait for rclk_period/2;
+   end process;
+ 
+   wclk_process :process
+   begin
+		wclk <= '0';
+		wait for wclk_period/2;
+		wclk <= '1';
+		wait for wclk_period/2;
+   end process;
+ 
+     stim_proc: process
+	   begin		
+		
+	 reset <= '1';
+		
+	 wait for 10 ns;
+		
+		reset <= '0';
+	 
+		wreq<='1';rreq<='0';
+		for i in 1 to 10 loop   --write 10 values to fifo.
+       datain <= conv_std_logic_vector(i,8);
+         wait for 10 ns;
+      end loop; 
+		
+		wreq<='0';rreq<='1';   --read 4 values from fifo
+		wait for 40 ns;
+		wreq<='0';rreq<='0';
+		wait for 100 ns;
+		
+		wreq<='1';rreq<='0';
+		for i in 1 to 10 loop   --write 10 values to fifo.
+       datain <= conv_std_logic_vector(i,8);
+         wait for 10 ns;
+      end loop; 
+
+		  for i in 1 to 10 loop   --write 10 values to fifo.
+       datain <= conv_std_logic_vector(i,8);
+         wait for 10 ns;
+      end loop; 
+		 
+		wait;
+		
+	
+	end process;
+END;
